1ce81f3c574d189a4365afa5de37fcc27b117388
danielricci <thedanny09@gmail.com>
2017-03-03 13:23:31 -0500

    Dialogs now take Windows as parents

--- Gosling/src/engine/core/mvc/view/DialogView.java
+++ Gosling/src/engine/core/mvc/view/DialogView.java
@@ -1,6 +1,5 @@
 package engine.core.mvc.view;
 
-import java.awt.Container;
 import java.awt.Window;
 
 import javax.swing.JDialog;
@@ -11,8 +10,8 @@ public abstract class DialogView extends JDialog implements IView {
 	
 	private final ViewProperties properties = new ViewProperties();
 	
-	public DialogView(Container container, String title, int width, int height) {
-		super((Window)container, title);
+	public DialogView(Window parent, String title, int width, int height) {
+		super(parent, title);
 		setSize(width, height);
 	}
 		
