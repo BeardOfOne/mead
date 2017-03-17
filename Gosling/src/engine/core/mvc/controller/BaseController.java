ee651b3a6e43bc47a00363fa4748996f0c62bab3
danielricci <thedanny09@gmail.com>
2017-03-16 22:59:01 -0400

    Selecting on a tile is now mutually exclusive
    - There needs to be a way for a registered event to be unregistered #35
    - Update operation at the end of the operations? #34

--- Gosling/src/engine/core/mvc/controller/BaseController.java
+++ Gosling/src/engine/core/mvc/controller/BaseController.java
@@ -24,15 +24,22 @@
 
 package engine.core.mvc.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import engine.api.IController;
 import engine.api.IView;
+import engine.util.event.ISignalListener;
 
 public abstract class BaseController implements IController  {
 
+	protected final Map<String, ISignalListener> SignalListenerMap = new HashMap<>();
+	
 	private IView _view;
 	
 	public <T extends IView> BaseController(T view) {
 		_view = view;
+		registerHandlers();
 	}
 	
 	protected final IView getView() {
@@ -42,5 +49,24 @@ public abstract class BaseController implements IController  {
 	@Override public void dispose() {
 		_view.dispose();
 		_view = null;		
+	}
+	
+	protected final String unregisterListener(ISignalListener listener) {
+		for(Map.Entry<String, ISignalListener> kvp : SignalListenerMap.entrySet()) {
+			if(kvp.getValue() == listener) {
+				SignalListenerMap.remove(kvp.getKey());
+				return kvp.getKey();
+			}
+		}
+		return null;
+	}
+	
+	protected final void registerListener(String signalName, ISignalListener listener) {
+		if(!SignalListenerMap.containsKey(signalName)) {
+			SignalListenerMap.put(signalName, listener);
+		}
+	}
+		
+	protected void registerHandlers() {
 	}
 }
