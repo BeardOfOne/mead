b253bf7fae2e96cc1e9f4e60497b5c79aeacd44f
Daniel Ricci <thedanny09@gmail.com>
2017-07-12 00:06:18 -0400

    partial commit for new navigation panel

---
+++ Gosling/src/engine/core/mvc/view/NavigationPanel.java
@@ -0,0 +1,109 @@
+/**
+* Daniel Ricci <thedanny09@gmail.com>
+*
+* Permission is hereby granted, free of charge, to any person
+* obtaining a copy of this software and associated documentation
+* files (the "Software"), to deal in the Software without restriction,
+* including without limitation the rights to use, copy, modify, merge,
+* publish, distribute, sublicense, and/or sell copies of the Software,
+* and to permit persons to whom the Software is furnished to do so, subject
+* to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+* IN THE SOFTWARE.
+*/
+
+package engine.core.mvc.view;
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.JPanel;
+import javax.swing.Scrollable;
+import javax.swing.SwingConstants;
+
+/**
+ * This panel gives the ability to be able to navigate content that is placed within this panel.
+ * 
+ * @author {@literal Daniel Ricci <thedanny09@gmail.com>}
+ *
+ */
+public class NavigationPanel extends JPanel implements Scrollable, MouseMotionListener {
+
+    private int _unitIncrement = 1;
+
+    public NavigationPanel(int unitIncrement) {
+        _unitIncrement = unitIncrement;
+
+        setOpaque(true);
+        setAutoscrolls(true);
+        addMouseMotionListener(this);
+    }
+
+    public void setMaxUnitIncrement(int pixels) {
+        _unitIncrement = pixels;
+    }
+
+    @Override public void mouseDragged(MouseEvent e) {
+        Rectangle r = new Rectangle(e.getX(), e.getY(), 1, 1);
+        scrollRectToVisible(r);
+    }
+
+    @Override public void mouseMoved(MouseEvent e) {
+    }
+
+    @Override public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
+        if (orientation == SwingConstants.HORIZONTAL) {
+            return visibleRect.width - _unitIncrement;
+        } else {
+            return visibleRect.height - _unitIncrement;
+        }
+    }
+
+    @Override public Dimension getPreferredScrollableViewportSize() {
+        return getPreferredSize();
+    }
+
+    @Override public Dimension getPreferredSize() {
+        return new Dimension(65536, 65536);
+    }
+
+    @Override public boolean getScrollableTracksViewportWidth() {
+        return false;
+    }
+
+    @Override public boolean getScrollableTracksViewportHeight() {
+        return false;
+    }
+
+    @Override public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
+        // Get the current position.
+        int currentPosition = 0;
+    
+        if (orientation == SwingConstants.HORIZONTAL) {
+            currentPosition = visibleRect.x;
+        } else {
+            currentPosition = visibleRect.y;
+        }
+    
+        // Return the number of pixels between currentPosition
+        // and the nearest tick mark in the indicated direction.
+        if (direction < 0) {
+            int newPosition = currentPosition - (currentPosition / _unitIncrement) * _unitIncrement;
+            return (newPosition == 0) ? _unitIncrement : newPosition;
+        } else {
+            return ((currentPosition / _unitIncrement) + 1) * _unitIncrement - currentPosition;
+        }
+    }
+}
