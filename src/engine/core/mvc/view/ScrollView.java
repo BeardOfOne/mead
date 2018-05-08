f3824135e8e502e92328366c9a8540b1d2106d67
Daniel Ricci <thedanny09@gmail.com>
2018-05-08 14:17:25 -0400

    cleaned up formatting of code

--- src/engine/core/mvc/view/ScrollView.java
+++ src/engine/core/mvc/view/ScrollView.java
@@ -1,26 +1,26 @@
 /**
-* Daniel Ricci <thedanny09@gmail.com>
-*
-* Permission is hereby granted, free of charge, to any person
-* obtaining a copy of this software and associated documentation
-* files (the "Software"), to deal in the Software without restriction,
-* including without limitation the rights to use, copy, modify, merge,
-* publish, distribute, sublicense, and/or sell copies of the Software,
-* and to permit persons to whom the Software is furnished to do so, subject
-* to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-* IN THE SOFTWARE.
-*/
+ * Daniel Ricci <thedanny09@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
 package engine.core.mvc.view;
 
@@ -90,13 +90,13 @@ public class ScrollView extends PanelView implements Scrollable, MouseMotionList
     @Override public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
         // Get the current position.
         int currentPosition = 0;
-    
+
         if (orientation == SwingConstants.HORIZONTAL) {
             currentPosition = visibleRect.x;
         } else {
             currentPosition = visibleRect.y;
         }
-    
+
         // Return the number of pixels between currentPosition
         // and the nearest tick mark in the indicated direction.
         if (direction < 0) {
@@ -107,18 +107,12 @@ public class ScrollView extends PanelView implements Scrollable, MouseMotionList
         }
     }
 
-	@Override public void initializeComponents() {
-	}
+    @Override public void initializeComponents() {
+    }
 
-	@Override public void initializeComponentBindings() {
-	}
+    @Override public void initializeComponentBindings() {
+    }
 
-    /* (non-Javadoc)
-     * @see engine.core.mvc.IDestructor#clear()
-     */
-    @Override
-    public void clear() {
-        // TODO Auto-generated method stub
-        
+    @Override public void clear() {
     }
 }
